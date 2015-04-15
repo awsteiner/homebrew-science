b8c18c17da2abd522d181a07fc66174302ece77d
Andrew W. Steiner <awsteiner@mykolab.com>
2015-04-15 14:21:48 -0400

    Updated tarball location and 'make check' target.

--- o2scl.rb
+++ o2scl.rb
@@ -1,8 +1,8 @@
 
 class O2scl < Formula
   homepage "http://o2scl.sourceforge.net"
-  url "http://23.115.64.197/o2scl-0.918.tar.gz"
-  sha256 "2b5ee94f937472a1a99103f6319f3bebb9f9e0f9118d2870e5f8177ac999586a"
+  url "http://web.utk.edu/~asteine1/o2scl-0.918.tar.gz"
+  sha256 "ef4f7caf448478e2ae33714b36f12a10cd11b2ce54ae8a6f4197135954920630"
 
   option "with-check", "Run build-time tests"
   option "with-examples", "Run build-time examples"
@@ -18,9 +18,9 @@ class O2scl < Formula
                           "--prefix=#{prefix}"
     system "make"
     system "make", "install"
-    system "make", "o2scl-test" if build.with? "check"
+    system "make", "check" if build.with? "check"
     system "make", "o2scl-examples" if build.with? "check"
-    # Would be nice to save the output for o2scl-test and
+    # Would be nice to save the output for make check and
     # o2scl-examples somewhere where the user can see it?
   end
 
