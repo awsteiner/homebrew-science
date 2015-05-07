31c2f392e9b44176e08997d01aa4bdfd483018e7
Andrew W. Steiner <awsteiner@mykolab.com>
2015-05-07 12:18:53 -0400

    Updated for eigen and armadillo options.

--- o2scl.rb
+++ o2scl.rb
@@ -2,20 +2,49 @@
 class O2scl < Formula
   homepage "http://o2scl.sourceforge.net"
   url "http://web.utk.edu/~asteine1/o2scl-0.918.tar.gz"
-  sha256 "ef4f7caf448478e2ae33714b36f12a10cd11b2ce54ae8a6f4197135954920630"
+  sha256 "22c25d02779df3f8831230c61040e2ba635762795adb2941cd5d5ffe103e1e5b"
 
   option "with-check", "Run build-time tests"
   option "with-examples", "Run build-time examples"
+  option "with-armadillo", "Include armadillo support"
+  option "with-eigen", "Include eigen support"
 
   depends_on "gsl"
   depends_on "hdf5"
   depends_on "boost"
   depends_on "readline"
+  if build.with? "armadillo"
+    depends_on "armadillo"
+  end
+  if build.with? "eigen"
+    depends_on "eigen"
+  end
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    if build.with? "armadillo"
+      if build.with? "eigen"
+        system "./configure", "--disable-dependency-tracking",
+               "--enable-armadillo","--enable-eigen",
+               "--disable-silent-rules",
+               "--prefix=#{prefix}"
+      else
+        system "./configure", "--disable-dependency-tracking",
+               "--enable-armadillo",
+               "--disable-silent-rules",
+               "--prefix=#{prefix}"
+      end
+    else
+      if build.with? "eigen"
+        system "./configure", "--disable-dependency-tracking",
+               "--enable-eigen",
+               "--disable-silent-rules",
+               "--prefix=#{prefix}"
+      else
+        system "./configure", "--disable-dependency-tracking",
+               "--disable-silent-rules",
+               "--prefix=#{prefix}"
+      end
+    end
     system "make"
     system "make", "install"
     system "make", "check" if build.with? "check"
