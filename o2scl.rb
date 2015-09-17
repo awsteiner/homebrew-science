ee732914605661c1ad1daa41346c3f25ec17559c
awsteiner <awsteiner@mykolab.com>
2015-09-17 14:55:12 +0200

    Small fixes.

--- o2scl.rb
+++ o2scl.rb
@@ -1,11 +1,24 @@
 #
-# Notes: armadillo doesn't yet work because it requires gcc, so
-# in order to make it work one has to install hdf5 and boost
-# with gcc as well.
+# Todos:
+#
+# - Clean up Eigen integration. See install section below.
+#
+# - Working with the armadillo library doesn't yet work because it
+# requires gcc, so in order to make it work one has to install hdf5
+# and boost with gcc as well.
+#
+# - Create a directory structure for installing the examples?
+#
+# - Document why include.install Dir["include/o2scl/*.h"] is needed
+# below.
+#
+# - Consider also storing the output of make check and make
+# o2scl-examples so the user can see them easily without --verbose?
 #
 class O2scl < Formula
+  desc "Object-oriented Scientific Computing Library"
   homepage "http://web.utk.edu/~asteine1/o2scl"
-  head "http://github.com/awsteiner/o2scl.git"
+  head "https://github.com/awsteiner/o2scl.git"
   stable do
     url "https://github.com/awsteiner/o2scl/releases/download/v0.918/o2scl-0.918.tar.gz"
     sha256 "d5c3bafd57dca10ee9b7c8a60708ac0537dfe31eef4e88eafeacfd62b3ee674c"
@@ -23,9 +36,6 @@ class O2scl < Formula
   depends_on "readline"
   depends_on "armadillo" => :optional
   depends_on "eigen" => :optional
-  depends_on "autoconf" => :optional
-  depends_on "automake" => :optional
-  depends_on "libtool" => :optional
 
   if build.with? "armadillo"
     depends_on "armadillo"
@@ -34,9 +44,9 @@ class O2scl < Formula
     depends_on "eigen"
   end
   if build.head?
-    depends_on "autoconf"
-    depends_on "automake"
-    depends_on "libtool"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
   end
 
   # The main installation script
@@ -45,17 +55,20 @@ class O2scl < Formula
   # The current fix for this is to install o2scl into eigen3 also, so
   # that o2scl can find the Eigen directory. This should be improved
   # in the future.
+  #
   def install
     # If we're installing from the repo, then use GNU autotools to make
-    # the ./configure script
+    # the ./configure script. Note that libtool is often installed with
+    # a 'g' prefix by homebrew.
     if build.head?
-      system "mkdir", "m4"
-      system "glibtoolize --force"
+      mkdir "m4"
+      system "glibtoolize", "--force"
       system "aclocal"
       system "autoheader"
       system "autoconf"
       system "automake", "-a", "--gnu"
     end
+    # Install considering all of the various options
     if build.with? "armadillo"
       if build.with? "eigen"
         if build.with? "no-range-check"
@@ -64,7 +77,7 @@ class O2scl < Formula
           ENV["CXXFLAGS"] = "-I/usr/local/include/eigen3"
         end
         system "./configure", "--disable-dependency-tracking",
-               "--enable-armadillo","--enable-eigen",
+               "--enable-armadillo", "--enable-eigen",
                "--disable-silent-rules",
                "--prefix=#{prefix}"
       else
@@ -99,7 +112,7 @@ class O2scl < Formula
     end
     #
     # If there's no documentation, just add some blank files so
-    # that the install target succeeds
+    # that the install target succeeds.
     #
     if build.head?
       system "make", "blank-doc"
@@ -107,17 +120,15 @@ class O2scl < Formula
     system "make"
     system "make", "install"
     #
-    # Fixme: should document why this is necessary
+    # FIXME: should document why this is necessary
     #
     include.install Dir["include/o2scl/*.h"]
     system "make", "check" if build.with? "check"
     system "make", "o2scl-examples" if build.with? "examples"
-    # Would be nice to save the output for make check and
-    # o2scl-examples somewhere where the user can see it?
     share.install Dir["examples/ex*.cpp"] if build.with? "examples"
   end
 
   test do
-    system "acol --version"
+    system "acol", "--version"
   end
 end
