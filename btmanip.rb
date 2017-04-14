ec4d54ccb376aba029c72a089fbca9a111ea2f55
awsteiner <awsteiner@mykolab.com>
2017-04-14 10:26:58 -0400

    Created btmanip.rb.

---
+++ btmanip.rb
@@ -0,0 +1,21 @@
+#
+# Todos:
+#
+class btmanip < Formula
+  desc "BibTeX File Manipulation Utility"
+  homepage "http://web.utk.edu/~asteine1/btmanip"
+  head "https://github.com/awsteiner/btmanip.git"
+
+  depends_on "o2scl"
+
+  # The main installation script
+  #
+  def install
+    #
+    system "make"
+  end
+
+  test do
+    system "btmanip", "--version"
+  end
+end
