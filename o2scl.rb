85cb240e4036845a02ad5ba96aaf540d1321d420
Andrew W. Steiner <awsteiner@mykolab.com>
2020-11-13 11:50:38 -0800

    Updated o2scl for 0.925.

--- o2scl.rb
+++ o2scl.rb
@@ -20,8 +20,8 @@ class O2scl < Formula
   homepage "https://isospin.roam.utk.edu/static/code/o2scl"
   head "https://github.com/awsteiner/o2scl.git"
   stable do
-    url "https://github.com/awsteiner/o2scl/releases/download/v0.924/o2scl-0.924.tar.gz"
-    sha256 "a9fa7b313566fd49d60fc3e924c207e0d1eb77f324cf982f7062a0451cbfea0b"
+    url "https://github.com/awsteiner/o2scl/releases/download/v0.925/o2scl-0.925.tar.gz"
+    sha256 "0e9818db200db6fb896aae0395e6510dc2f8d97d532e903994cabc4680617385"
   end
 
   option "with-check", "Run build-time tests"
