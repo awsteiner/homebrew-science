1e30f0b9a8fd2e200da441f488ac55bc27b0f06e
Andrew W. Steiner <awsteiner@mykolab.com>
2023-06-09 15:00:02 -0400

    Work on o2scl.

--- o2scl.rb
+++ o2scl.rb
@@ -75,10 +75,12 @@ class O2scl < Formula
     end
 
     system "echo", "e0"
-
     ENV["ETEST"] = "one"
+    system "echo", "em1"
     ENV["ETEST"] = "two", "$ETEST"
+    system "echo", "em2"
     system "echo", "$ETEST"
+    system "echo", "em3"
     
     # Install considering all of the various options
     if build.with? "armadillo"
